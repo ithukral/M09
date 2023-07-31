288b55e5c71c23983cd246145572513b844d64a7
Isha Thukral <69432538+ithukral@users.noreply.github.com>
2023-07-31 18:53:50 -0400

    Create setup.py
    change

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='HW9',
+
+    version='1.0.0',
+
+    url='https://github.com/ithukral/M09',
+
+    author='Isha Thukral',
+
+    author_email='van8me@virginia.edu',
+
+    description='Description of my package',
+
+    packages=find_packages(),    
+
+)
